34c5e12105a31a6f41e4f52f4ee27160dc0a9603
dilmyImesha <dilmiimesha11@gmail.com>
2024-03-08 23:49:12 +0530

    create Q1 and add comments

--- Q1.java
+++ Q1.java
@@ -1,9 +1,10 @@
 public class Inky {
 
+    //constructor with parameter
     public Inky (String Pinkey){
         System.out.println(Pinkey);
     }
-
+    //construtor with three parameters
     public Inky (String A, String B, String C){
         System.out.println(B+" "+A);
         System.out.println(A+" "+C);
